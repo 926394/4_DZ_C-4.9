98ebd7edcaa670a56cc0f6c7933d3e47e249ccb1
Nikolay <meta1mang@mail.ru>
2023-01-15 01:41:51 +0300

    .\Task_66_numMandN\

---
+++ Task_66_numMandN/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.Clear();
+Console.Write("Здравствуйте! Ниже, введите диапазон слагаемых");
+Console.WriteLine();
+Console.Write("Введите первое слагаемое: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе слагаемое: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumNumMandN(int first, int and)
+{
+    if (first == and) return first;
+
+    return (first + SumNumMandN(first + 1, and));
+}
+
+Console.WriteLine(SumNumMandN(M, N));
